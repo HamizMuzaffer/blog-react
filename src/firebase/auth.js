bc311ba6d1bdd0c19151a1e1617cdb2597c4e902
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-05-17 12:00:20 -0700

    Database added

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -9,19 +9,60 @@ import {
     onAuthStateChanged,
     signOut,
     updateProfile
-  
-  } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-  
-  const auth = getAuth(app);
 
-  export const creatuserbySignUp = () => {
-  
-  
-    createUserWithEmailAndPassword(auth, email, passoword,username)
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
-      .then((userCredential) => {     
-        const user = userCredential.user;
-      })
-    }  
+const auth = getAuth(app);
 
-    
+export const creatuserbySignUp = () => {
+
+
+    createUserWithEmailAndPassword(auth, email, passoword, username)
+
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        })
+}
+
+
+export const userLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
+export const userLogOut = ( ) =>
+{
+
+    signOut(auth)
+    .then(() => {
+      window.location.href = "./login.html"
+    }).catch((error) => {
+
+        console.error(error)
+})}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/firebase/services.js
@@ -0,0 +1,10 @@
+import { app } from "../Config/Config";
+
+
+/*
+ADD BLOG 
+READ BLOG
+UPDATE BLOG
+DELETE BLOG
+
+*/
